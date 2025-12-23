0895b883cedc2967c0d0ec178a2bd002330ece5f
Draxicon <16972292+Draxicon@user.noreply.github.com>
2025-12-23 00:02:16 -0600

    test with button (no debounce)

---
+++ src/include/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+// left/Right Control button pins
+#define LEFTBUTTON 12
+#define RIGHTBUTTON 13
+
+
+#endif
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
+#include "config.h"
 // #include "tft_test.h"// uncomment if testing tft screen
 
-
+u_int8_t lastState = LOW;
+u_int8_t currentState;
 
 void setup()
 {
 //  basicdraw(); // uncomment if testing tft screen
-
+    Serial.begin(9600);
+    pinMode(LEFTBUTTON, INPUT_PULLUP);
 
 }
 
 void loop()
 {
+    currentState = digitalRead(LEFTBUTTON);
 
+    if(lastState == HIGH && currentState == LOW)
+    {
+        Serial.println("The button is pressed");
+    }
+    else if (lastState == LOW && currentState == HIGH)
+    {
+        Serial.println("The button is released");
+    }
+
+    lastState = currentState;
 }
