14cd149cbbf9271cb3bf1ff8bd06cd70b882527a
Draxicon <169872292+Draxicon@users.noreply.github.com>
2025-12-26 14:29:55 -0600

    Create intital structure for input Manager

---
+++ src/include/inputManager.h
@@ -0,0 +1,41 @@
+/*
+* Input manager library, used to help define functionality of our inputs (rotary encoder, buttons, ect)
+* Drax: 12.24.2025 created class for rotary encoder
+*
+*/
+
+#ifndef INPUT_MANAGER_H
+#define INPUT_MANAGER_H
+
+#include <Arduino.h>
+
+/*
+* \brief used to help get current direction of the 
+* rotary encoder if it is in motion
+* \param 
+*/
+class RotaryEncoder
+{
+    public:
+        enum class Direction
+        {
+
+        }
+
+    private:
+}
+/*
+* \brief 
+*
+*
+*/
+class Button
+{
+    public:
+
+    private:
+
+}
+
+
+#endif
