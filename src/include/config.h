e9ca34bf5888b09bfa771aa78773abbe9a822897
Draxicon <169872292+Draxicon@users.noreply.github.com>
2026-01-12 20:48:49 -0600

    update pinouts

--- src/include/config.h
+++ src/include/config.h
@@ -6,8 +6,8 @@
 #define RIGHTBUTTON 13
 
 // First pin for Rotary encoder, Clockwise Rotations
-#define RotaryEncoderPinA 14
+#define RotaryEncoderPinA 10
 // Second pin for Rotary encoder, Counter-Clockwise Rotations
-#define RotaryEncoderPinB 27
+#define RotaryEncoderPinB 11
 
 #endif
