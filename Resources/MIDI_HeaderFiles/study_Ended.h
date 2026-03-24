1233afd78abae32dc2ebeb1ab584cc37760e8455
Pepsi <84537032+Prepsi@users.noreply.github.com>
2026-03-24 12:59:56 -0500

    added Study + Break ended header files

---
+++ Resources/MIDI_HeaderFiles/study_Ended.h
@@ -0,0 +1,35 @@
+char * Break_End[13]
+{
+	"G4",
+	"E4",
+	"C4",
+	"F4",
+	"D4",
+	"B3",
+	"E4",
+	"C4",
+	"A3",
+	"D4",
+	"B3",
+	"G3",
+	"C4"
+};
+
+int Break_Endlen = sizeof(Break_End) / sizeof(Break_End[0]);
+
+float Break_Endtimes[13]
+{
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17,
+	0.17
+};
